63dd1975c9132b796035f7764d9d63a2d5bf1361
AppleKid7 <kvltd00d@gmail.com>
2016-12-03 17:20:26 -0500

    added method for getting prime factors and some tests

---
+++ Primes/GetPrimes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primes
+{
+    class GetPrimes
+    {
+        public List<int> getPrimes(int n) {
+            List<int> primes = new List<int>();
+            while (n % 2 == 0)
+            {
+                primes.Add(2);
+                n /= 2;
+            }
+            for(int i = 3; i <= Math.Sqrt(n); i += 2)
+            {
+                while(n % i == 0)
+                {
+                    primes.Add(i);
+                    n /= i;
+                }
+            }
+            if(n > 2)
+            {
+                primes.Add(n);
+            }
+            return primes;
+        }
+    }
+}
---
+++ Primes/TestGetPrimes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Primes
+{
+    [TestFixture]
+    class TestGetPrimes
+    {
+        [Test]
+        public void testGetPrimesSmall()
+        {
+            GetPrimes primes = new Primes.GetPrimes();
+            List<int> result = primes.getPrimes(5);
+            List<int> expected = new List<int>();
+            expected.Add(5);
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void testGetPrimesLarge()
+        {
+            GetPrimes primes = new Primes.GetPrimes();
+            List<int> result = primes.getPrimes(315);
+            List<int> expected = new List<int>();
+            expected.Add(3);
+            expected.Add(3);
+            expected.Add(5);
+            expected.Add(7);
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void testGetPrimesLargeDouble()
+        {
+            GetPrimes primes = new Primes.GetPrimes();
+            List<int> result = primes.getPrimes(630);
+            List<int> expected = new List<int>();
+            expected.Add(2);
+            expected.Add(3);
+            expected.Add(3);
+            expected.Add(5);
+            expected.Add(7);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
